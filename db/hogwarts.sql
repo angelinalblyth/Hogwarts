0327fd55b2bede734cf5b65810f4d434830e3c32
Angelina Blyth <angelinalblyth@hotmail.com>
2018-06-06 14:29:54 +0100

    added houses to db

--- db/hogwarts.sql
+++ db/hogwarts.sql
@@ -3,7 +3,14 @@ DROP TABLE students;
 
 CREATE TABLE houses(
   id serial4 PRIMARY KEY,
-  name VARCHAR(255)
+  name VARCHAR(255),
+  founder VARCHAR(255),
+  colours VARCHAR(255),
+  animal VARCHAR(255),
+  traits VARCHAR(255),
+  head VARCHAR(255),
+  ghost VARCHAR(255),
+  common_room VARCHAR(255),
 );
 
 CREATE TABLE students(
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,14 +5,51 @@ require('pry-byebug')
 
 Student.delete_all()
 
-house1= House.new({'name' => 'Griffindor'})
+house1= House.new({
+  'name' => 'Griffindor',
+  'founder' => 'Godric Gryffindor',
+  'colours' => 'Scarlet and gold',
+  'animal' => 'Lion',
+  'traits' => 'Brave',
+  'head' => 'Minerva McGonagall',
+  'ghost' => 'Nearly Headless Nick',
+  'common_room' => 'Gryffindor Tower' ,
+  })
 house1.save()
-house2= House.new({'name' => 'Hufflepuff'})
+house2= House.new({
+  'name' => 'Hufflepuff',
+  'founder' => 'Helga Hufflepuff',
+  'colours' => 'Yellow and black',
+  'animal' => 'Badger',
+  'traits' => 'Loyal',
+  'head' => 'Pomona Sprout',
+  'ghost' => 'Fat Friar',
+  'common_room' => 'Hufflepuff Basement' ,
+  })
 house2.save()
-house3= House.new({'name' => 'Ravenclaw'})
+house3= House.new({
+  'name' => 'Raveclaw',
+  'founder' => 'Rowena Ravenclaw',
+  'colours' => 'Blue and bronze',
+  'animal' => 'Eagle',
+  'traits' => 'Wise',
+  'head' => 'Horace Slughorn',
+  'ghost' => 'Grey Lady',
+  'common_room' => 'Ravenclaw Tower' ,
+  })
 house3.save()
-house4= House.new({'name' => 'Slytherin'})
+house4= House.new({
+  'name' => 'Slytherin',
+  'founder' => 'Salazar Slytherin',
+  'colours' => 'Green and silver',
+  'animal' => 'Snake',
+  'traits' => 'Cunning',
+  'head' => 'Severus Snape',
+  'ghost' => 'Bloody Baron',
+  'common_room' => 'Slytherin Dungeon' ,
+  })
 house4.save()
+
 
 student1 = Student.new({'first_name' => 'Harry','last_name' => 'Potter','house' => 'Gryffindor','age' => '11'})
 student1.save()
