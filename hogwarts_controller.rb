1ac6e1930ada3f7151aebc7fe6fa9910cc675de9
Angelina Blyth <angelinalblyth@hotmail.com>
2018-06-06 13:15:17 +0100

    View working

--- hogwarts_controller.rb
+++ hogwarts_controller.rb
@@ -1,5 +1,12 @@
-require('sinatra')
-require('sinatra/contrib/all')
-require('pry-byebug')
-require_relative('../models/student')
-also_reload('../models/*')
+require( 'sinatra' )
+require( 'sinatra/contrib/all' )
+require( 'pry-byebug' )
+require_relative('./models/student')
+also_reload('./models/*')
+
+get '/students' do
+
+  @students = Student.all()
+  erb (:index)
+
+end
